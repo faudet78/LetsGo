024540f3b654deddf83367e0d3a73fb7da8bcf92
azeredudu1 <azeredudu1@gmail.com>
2015-10-29 22:10:05 +0100

    Base de donnee, issue reglee.

--- siteCovoiturage/src/main/java/com/site/covoiturage/services/InitDbService.java
+++ siteCovoiturage/src/main/java/com/site/covoiturage/services/InitDbService.java
@@ -1,6 +1,5 @@
 package com.site.covoiturage.services;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,13 +10,11 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.site.covoiturage.model.Annonce;
 import com.site.covoiturage.model.Reservation;
 import com.site.covoiturage.model.Role;
-import com.site.covoiturage.model.User;
 import com.site.covoiturage.repositories.AnnonceRepository;
 import com.site.covoiturage.repositories.ReservationRepository;
 import com.site.covoiturage.repositories.RoleRepository;
@@ -64,7 +61,7 @@ public class InitDbService {
 
 	@PostConstruct
 	public void init() {
-		if (userRepository.findAll().size() == 0) {
+		if (roleRepository.findAll().size() == 0) {
 			Role role1 = new Role();
 			role1.setName("ROLE_USER");
 			roleRepository.save(role1);
@@ -72,37 +69,39 @@ public class InitDbService {
 			List<Role> roles = new ArrayList<Role>();
 			roles.add(role1);
 
-			User user = new User();
-			user.setActive(true);
-			user.setDateInscription(new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
-			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-			user.setPassword(encoder.encode("christophe"));
-			user.setUsername("azeredudu");
-			user.setFirstName("Azere");
-			user.setLastName("Mabiala");
-			user.setCommentaire("Je suis un mec sympa, cool, .. ..");
-			user.setRoles(roles);
-			userRepository.save(user);
-
-			if (annonceRepository.findAll().size() == 0) {
-
-				Annonce annonce = new Annonce("Paris", "Lyon", 3L, 3L, true,
-						new SimpleDateFormat("dd/MM/yyyy").format(new Date()),
-						new SimpleDateFormat("HH:mm").format(new Date()), 25, "Allee simple", "Mercedes", "N'importe ou",
-						"N'importe ou", "Je pars sur Lyon visiter des parents j'ai trois places de libre dans ma voiture",
-						true, true, null, false, 3, 1, 3, false, new SimpleDateFormat("dd/MM/yyyy").format(new Date()),
-						"Classe c", user);
-				annonceRepository.save(annonce);
-
-				Annonce annonce1 = new Annonce("Rennes", "Bordeaux", 3L, 3L, true,
-						new SimpleDateFormat("dd/MM/yyyy").format(new Date()),
-						new SimpleDateFormat("HH:mm").format(new Date()), 25, "Allee simple", "Mercedes", "N'importe ou",
-						"N'importe ou",
-						"Je pars sur Bordeaux visiter des parents j'ai trois places de libre dans ma voiture", true, true,
-						null, false, 4, 1, 4, false, new SimpleDateFormat("dd/MM/yyyy").format(new Date()), "Classe c", user);
-				annonceRepository.save(annonce1);
-
-			}
+			/*
+			 * User user = new User(); user.setActive(true);
+			 * user.setDateInscription(new
+			 * SimpleDateFormat("dd/MM/yyyy").format(new Date()));
+			 * BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			 * user.setPassword(encoder.encode("christophe"));
+			 * user.setUsername("azeredudu"); user.setFirstName("Azere");
+			 * user.setLastName("Mabiala");
+			 * user.setCommentaire("Je suis un mec sympa, cool, .. ..");
+			 * user.setRoles(roles); userRepository.save(user);
+			 * 
+			 * if (annonceRepository.findAll().size() == 0) {
+			 * 
+			 * Annonce annonce = new Annonce("Paris", "Lyon", 3L, 3L, true, new
+			 * SimpleDateFormat("dd/MM/yyyy").format(new Date()), new
+			 * SimpleDateFormat("HH:mm").format(new Date()), 25, "Allee simple",
+			 * "Mercedes", "N'importe ou", "N'importe ou",
+			 * "Je pars sur Lyon visiter des parents j'ai trois places de libre dans ma voiture"
+			 * , true, true, null, false, 3, 1, 3, false, new
+			 * SimpleDateFormat("dd/MM/yyyy").format(new Date()), "Classe c",
+			 * user); annonceRepository.save(annonce);
+			 * 
+			 * Annonce annonce1 = new Annonce("Rennes", "Bordeaux", 3L, 3L, true,
+			 * new SimpleDateFormat("dd/MM/yyyy").format(new Date()), new
+			 * SimpleDateFormat("HH:mm").format(new Date()), 25, "Allee simple",
+			 * "Mercedes", "N'importe ou", "N'importe ou",
+			 * "Je pars sur Bordeaux visiter des parents j'ai trois places de libre dans ma voiture"
+			 * , true, true, null, false, 4, 1, 4, false, new
+			 * SimpleDateFormat("dd/MM/yyyy").format(new Date()), "Classe c",
+			 * user); annonceRepository.save(annonce1);
+			 * 
+			 * }
+			 */
 
 		}
 
